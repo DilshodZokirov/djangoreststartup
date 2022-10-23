5caef129dc92ff1c45df806d80a98322a97e30b3
dz <dilshodzokirov03@gmail.com>
2022-10-23 21:31:49 +0500

    Hello2

--- api/products/urls.py
+++ api/products/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from api.products.views.agent import ProductModelViewSet
+
 router = DefaultRouter()
+router.register("worker", ProductModelViewSet)
 urlpatterns = [
     path("", include(router.urls))
 
