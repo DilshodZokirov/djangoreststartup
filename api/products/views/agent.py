c073f7cda3027a2cad231b9cae257fdd1232d8a0
dz <dilshodzokirov03@gmail.com>
2022-11-14 21:14:29 +0500

    New Updated Order Product Agent

--- api/products/views/agent.py
+++ api/products/views/agent.py
@@ -23,7 +23,7 @@ class ProductModelViewSet(ModelViewSet):
         self.queryset = Product.objects.filter(company=request.user.company)
         return super(ProductModelViewSet, self).list(request, *args, **kwargs)
 
-    def partial_update(self, request, *args, **kwargs):
+    def update(self, request, *args, **kwargs):
         self.serializer_class = UpdateProductClassSerializer
         serializer = self.get_serializer(self.get_object(), data=request.data)
         serializer.is_valid(raise_exception=True)
