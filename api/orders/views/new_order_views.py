85843004167e705d332c65511199422903ce8f02
dz <dilshodzokirov03@gmail.com>
2022-11-15 03:18:26 +0500

    New Updated Order Product Agent

--- api/orders/views/new_order_views.py
+++ api/orders/views/new_order_views.py
@@ -71,6 +71,7 @@ class OrderClientModelViewSet(ModelViewSet):
     def retrieve(self, request, *args, **kwargs):
         self.serializer_class = DetailOrderSerializer
         serializer = self.get_serializer(self.get_object(), data=request.data)
+        serializer.is_valid(raise_exception=True)
         return Response(serializer.data)
 
     def update(self, request, *args, **kwargs):
