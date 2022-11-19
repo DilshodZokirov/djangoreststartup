25e2df47827975746cc880936957b7913e2fbab8
dz <dilshodzokirov03@gmail.com>
2022-11-19 17:37:03 +0500

    New Updated Order Product Agent

--- apps/users/models/__init__.py
+++ apps/users/models/__init__.py
@@ -10,7 +10,10 @@ class CustomUserManager(BaseUserManager):
     def _create_user(self, phone_number, password, **extra_fields):
         now = timezone.now()
         if not phone_number:
-            raise ValueError('The given phone_number must be set')
+            raise ValueError({"message": {
+                "uz": "Iltimos telefon nomerni kiriting",
+                "ru": "Указанный номер телефона должен быть установлен",
+                "en": 'The given phone number must be set'}})
         user = self.model(phone_number=phone_number, is_active=True, last_login=now,
                           date_joined=now, **extra_fields)
         user.set_password(password)
